7cef234cb41c5fc290b2dbcc878a1b0302ad58d0
HusniNaufal <hsnuniversity23@gmail.com>
2025-06-01 10:37:19 +0700

    modified:   src/layouts/Mainlayout.jsx

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -3,7 +3,7 @@ import FooterComponent from "./Footer";
 export default function Mainlayout({ children }) {
   return (
     <>
-      <div className="flex flex-col min-h-[200vh]">
+      <div className="flex flex-col min-h-[100vh]">
         <HeaderComponent />
         <div className="flex-grow px-16">{children}</div>
         <FooterComponent />
